7a8063d87aee7365928396042f20e56155c00a11
jontiveros@chicagotechacademy.org <jontiveros@chicagotechacademy.org>
2013-12-14 12:40:25 -0600

    12

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AppJj::Application.config.secret_key_base = 'b9a378a9f681ec235a40dffae7a00171a2e1745840d690c9dc7b6da8923361f299410deff054f9c7a6614a7823cda4918c459d2d259c616847826341ae193b27'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AppJj::Application.config.session_store :cookie_store, key: '_app_jj_session'
